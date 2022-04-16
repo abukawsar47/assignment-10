a7124730a431ce26c631a4972ee954e88b91b4f9
abukawsar47 <abukawsar47ak@gmail.com>
2022-04-17 00:08:39 +0600

    Project Initialized

---
+++ src/Pages/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './About.css';
+
+const About = () => {
+    return (
+        <div>
+            <h2>About Page</h2>
+        </div>
+    );
+};
+
+export default About;
