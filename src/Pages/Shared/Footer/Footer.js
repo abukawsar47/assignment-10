9dac554b3ec5015d312a84cb515c451d098de25c
abukawsar47 <abukawsar47ak@gmail.com>
2022-04-17 01:52:30 +0600

    Set Footer

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Footer.css'
 
 const Footer = () => {
+    const today = new Date();
+    const year = today.getFullYear();
     return (
-        <div>
-            <h1>Footer</h1>
-        </div>
+        <footer className='text-center bg-dark text-white mt-5 py-4 position-fixed bottom-0 w-100'>
+            <p className='m-0'><small>Copyright &copy; {year} <Link className='text-decoration-none text-danger fw-bold' to={"home"}>Luvfit</Link> All Rights Reserved</small></p>
+        </footer >
     );
 };
 
